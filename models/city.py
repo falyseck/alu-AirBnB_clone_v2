ea91b8d83d463b2212a1bc00d44f09a9f7211a73
falyseck <169052940+falyseck@users.noreply.github.com>
2025-01-22 16:18:59 +0000

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""This is the City module."""
+""" City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """This is the City class."""
+    """ The city class, contains state ID and name """
+
     __tablename__ = 'cities'
 
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship("Place", backref="cities", cascade="all, delete-orphan")
+    places = relationship("Place", backref="cities")
