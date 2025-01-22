c6a54c7cf6f728151a5d98a0cb6eb87f523d8711
falyseck <169052940+falyseck@users.noreply.github.com>
2025-01-22 16:18:02 +0000

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""This module defines the Amenity class"""
+""" State Module for HBNB project """
+import os
+
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
-    """Amenity class to store amenities information"""
+    """Amenities of a place"""
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-
-    # Relationship with Place for Many-To-Many
-    place_amenities = relationship("Place", secondary=place_amenity, back_populates="amenities")
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        place_amenities = relationship('Place', secondary="place_amenity")
